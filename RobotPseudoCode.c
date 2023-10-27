e5c1a5fc6d1f79c22936f3b99af63da0ab182b06
AshTheEngiqueer <a7lang@uwaterloo.ca>
2023-10-27 12:17:20 -0400

    Update RobotPseudoCode.c
    Added some more work on handrail algorithm

--- RobotPseudoCode.c
+++ RobotPseudoCode.c
@@ -4,11 +4,17 @@ array handrailAlgo(array mazeMap)
 	//Define solution array as an array of points in reference frame of maze cells (ex. 1-20 for a 20 cell maze)
 	//Solution will be realized by sequence of movements from point to point
 	//Solve Maze while recording movements
-	int currentCellX = 0, currentCellY = 0;
+	int currentCellX = 0, currentCellY = 0, goalCellX = 0, goalCellY = 0;
+	bool solved = False;
 
 	//search mazeMap for start and end points
 
 	//pick a start and end (doesnt matter tbh)
+	while (!solved)
+	{
+
+	}
+	//Check if solved
 
 	simpleSolArray = simplify(solArray);
 
@@ -16,6 +22,11 @@ array handrailAlgo(array mazeMap)
 }
 */
 
+
+/*
+Inputs: EV3 Buttons, Colour Sensor, Ultrasonic or Touch Sensor, Motor Encoders
+Outputs: 3x motor (x-axis, z-axis, pen)
+*/
 task main()
 {
 
