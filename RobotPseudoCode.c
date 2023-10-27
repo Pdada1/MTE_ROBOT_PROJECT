d47f5dea22ddfa6ecfd09257f26c05ca494a5f7e
AshTheEngiqueer <a7lang@uwaterloo.ca>
2023-10-27 11:38:26 -0400

    Create RobotPseudoCode.c
    Started Pseudocode for handrail algorithm

---
+++ RobotPseudoCode.c
@@ -0,0 +1,22 @@
+/*
+array handrailAlgo(array mazeMap)
+{
+	//Define solution array as an array of points in reference frame of maze cells (ex. 1-20 for a 20 cell maze)
+	//Solution will be realized by sequence of movements from point to point
+	//Solve Maze while recording movements
+	int currentCellX = 0, currentCellY = 0;
+
+	//search mazeMap for start and end points
+
+	//pick a start and end (doesnt matter tbh)
+
+	simpleSolArray = simplify(solArray);
+
+	return simpleSolArray;
+}
+*/
+
+task main()
+{
+
+}
