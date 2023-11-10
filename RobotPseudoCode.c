993973c5b3c6dfefde2450af0d3709acbaa1dfde
AshTheEngiqueer <a7lang@uwaterloo.ca>
2023-11-10 10:06:18 -0500

    Update RobotPseudoCode.c

--- RobotPseudoCode.c
+++ RobotPseudoCode.c
@@ -1,6 +1,12 @@
 void handrailAlgo();
-void searchGoals();
+void searchEnds(int &startCellX, int &startCellY, int &goalCellX, int &goalCellY);
+void readMaze(); //this is fairly straightforward
+void moveToCell(int currentCellX, int currentCellY, int moveToCellX, int moveToCellY); //this is fairly straightforward, use controlXMotors()
+void controlXMotors(); //this is trivial
 
+
+const int MAZE_R = 41, MAZE_C = 41, MOTOR_POWER = 30;
+int mazeMap[MAZE_R][MAZE_C] = {0};
 task main()
 {
 /*
@@ -19,7 +25,8 @@ void handrailAlgo()
 	int currentCellX = 0, currentCellY = 0, startCellX = 0, startCellY = 0, goalCellX = 0, goalCellY = 0;
 
 	//search mazeMap for start and end points
-	searchGoals(goalCellX, goalCellY);
+	searchEnds(startCellX, startCellY, goalCellX, goalCellY);
+
 	int facing = 0;
 	char directions[4] = {E, N, S, W};
 
