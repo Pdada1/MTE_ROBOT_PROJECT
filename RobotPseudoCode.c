aae93b75a6ef89fbed2147e49e471dcfbd9ebf07
little-charchar <149853306+little-charchar@users.noreply.github.com>
2023-11-13 01:38:12 -0500

    Update RobotPseudoCode.c

--- RobotPseudoCode.c
+++ RobotPseudoCode.c
@@ -33,29 +33,30 @@ void searchEnds(int &startCellX, int &startCellY, int &goalCellX, int &goalCellY
 	//only scan the upper row from left to right
 	//assume we start at the upper left corner and the colour sensor is already at the 1st tile (we can edit this if we need to)
 	
-	//while we move the robot to the right:
-	nMotorEncoder[motorA] = 0; //is this needed?
-	const int MOVE_TO_END = MAZE_C-2; //move directly to the 2nd last colomn from the right
-	
+	nMotorEncoder[motorA] = 0;
+	const int MOVE_TO_END = MAZE_C-2;
+
+	//move directly to the 2nd last colomn from the right
 	motor[motorA] = motor[motorB] = MOTOR_POWER;
     	while(nMotorEncoder[motorA] < MOVE_TO_END*CELL_TO_ENCODER)
-    		{
-		if(SensorValue [S1] == 6) //if we detect the white start tile at the 2nd last colomn from the right
-			{
-				startCellX = MAZE_C-2; //in the 2nd last colomn from the right
-				startCellY = 0; //in the 1st row from the top
-				goalCellX = 1; //in the 2nd colomn from the left
-				goalCellY = MAZE_R-1; //in the 1st row from the bottom
-			}
-		else
-			{
-				startCellX = 0; //in the 1st colomn from the left
-				startCellY = 1; //in the 2nd row from the top
-				goalCellX = MAZE_C-1; //in the 1st colomn from the right
-				goalCellY = MAZE_R-2; //in the 2nd row fron the bottom
-			}
+    	{}
+   	motor[motorA] = motor[motorB]=0;
+
+	if(SensorValue [S1] == 6) //if we detect the white start tile at the 2nd last colomn from the right
+		{
+			startCellX = MAZE_C-2; //in the 2nd last colomn from the right
+			startCellY = 0; //in the 1st row from the top
+			goalCellX = 1; //in the 2nd colomn from the left
+			goalCellY = MAZE_R-1; //in the 1st row from the bottom
 		}
-   	 motor[motorA] = motor[motorB]=0;
+	else
+		{
+			startCellX = 0; //in the 1st colomn from the left
+			startCellY = 1; //in the 2nd row from the top
+			goalCellX = MAZE_C-1; //in the 1st colomn from the right
+			goalCellY = MAZE_R-2; //in the 2nd row fron the bottom
+		}
+	
 
 	//returns back to the initial position
 	motor[motorA] = motor[motorB] = -1*MOTOR_POWER;
