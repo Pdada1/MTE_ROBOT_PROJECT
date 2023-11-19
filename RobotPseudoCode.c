5944b7d593ef612ca7ea7f5eb469c99ebf11710a
Pranav Mahabal <95592003+Pdada1@users.noreply.github.com>
2023-11-18 23:10:15 -0500

    Magic Values Reset

--- RobotPseudoCode.c
+++ RobotPseudoCode.c
@@ -141,7 +141,7 @@ void moveToCell(int &currentCellX, int &currentCellY, int nextCellX, int nextCel
     int iEncodeXB = nMotorEncoder[motorB]; //not sure if this line is necessary
     int iEncodeY = nMotorEncoder[motorC];
     int dEncodeX = (nextCellX - currentCellX) * CELL_TO_ENCODER*1.83871;//magic number
-    int dEncodeY = (nextCellY - currentCellY) * CELL_TO_ENCODER*96.66;
+    int dEncodeY = (nextCellY - currentCellY) * CELL_TO_ENCODER*58;
     //move the x distance
     if (currentCellX > nextCellX)
     {
